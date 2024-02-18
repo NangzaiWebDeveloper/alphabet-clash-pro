d080766db97fab290fc979c12a4e00a83f8e292f
NangzaiWebDeveloper <abujamnangzai00@gmail.com>
2024-02-18 20:22:09 +0600

    this is mine

---
+++ js/callFunction.js
@@ -0,0 +1,11 @@
+//Start Button call by function
+function start(){
+    //1st step
+    hideElementByID('home-screen')//press & add home screen
+
+    //2nd step
+    showElementByID('play-ground')//remove hidden & show play ground
+
+    //3rd step
+    continueGame()//(generate random alphabet) (insert the innerText) (add the bg color in keyboard button)
+}
---
+++ js/logicOfAlpha.js
@@ -0,0 +1,43 @@
+//process-1
+function hideElementByID(element){//press and hide element
+    const homeScreen = document.getElementById(element)
+    homeScreen.classList.add('hidden')
+}
+//process-2
+function showElementByID(element){//show play ground
+    const playGround = document.getElementById(element)
+    playGround.classList.remove('hidden')
+}
+//process-3
+function setBgColorAlphabetById(elementId){// add alphabet bg color
+    const setBg = document.getElementById(elementId)
+    setBg.classList.add('bg-green-700')
+}
+
+
+//make Randomly generate a Alphabet functions
+function getARandomAlphabet(){
+
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'//String
+    const getArray = alphabetString.split('')//string convert to array
+
+    const randomArr = Math.random() * 25//generate random number
+    const index = Math.round(randomArr)//convert integer number
+
+    const randomAlphabet = getArray[index]//get only Alphabet
+    return randomAlphabet;
+}
+
+
+//(generate random alphabet) (insert the innerText) (add the bg color in keyboard button)
+function continueGame(){
+    //1st-- generate a random alphabet
+    const alphabet = getARandomAlphabet()
+
+    //2nd-- call mutable tag & insert the innerText
+    const currentText = document.getElementById('current-show-text')//ID call from randomly generate element
+    currentText.innerText = alphabet//randomly generate alphabet show to the screen
+
+    //3rd-- add background Color in Button
+    setBgColorAlphabetById(alphabet)
+}
